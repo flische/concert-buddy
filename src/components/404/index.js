8ac79fb8d541dded5005bb5454f0ec71be2a7472
Howard Kim <howardkim820@gmail.com>
2018-08-20 16:31:42 -0700

    404 error

---
+++ src/components/404/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+            <Link to ="/">Return To Home Page </Link>
+        </div>
+    )
+}
