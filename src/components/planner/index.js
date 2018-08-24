8affd0bd51ed83b777988f7ca702f3c2b4ba87a3
flische <flische@gmail.com>
2018-08-24 16:29:32 -0700

    completed planner homepage setup with retrieving concert data

--- src/components/planner/index.js
+++ src/components/planner/index.js
@@ -1,9 +1,64 @@
 import React, { Component } from 'react';
-import './planner.css'
-
+import { connect } from 'react-redux';
+import { get_concert_details } from '../../actions';
+import './planner.css';
 
 class Planner extends Component {
+
+    convertTime = (militaryTime) => {
+        if (!militaryTime) {
+            return;
+        }
+        var time = militaryTime;
+        time = time.split(':');
+        var hours = Number(time[0]);
+        var minutes = Number(time[1]);
+        var seconds = Number(time[2]);
+        var timeValue;
+        if (hours > 0 && hours <= 12) {
+            timeValue = "" + hours;
+        } else if (hours > 12) {
+            timeValue = "" + (hours - 12);
+        } else if (hours == 0) {
+            timeValue = "12";
+        }
+        timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;
+        timeValue += (hours >= 12) ? " P.M." : " A.M.";
+        return timeValue;
+    }
+
+    convertDateFormat = (yyddmm) => {
+        var newDate = yyddmm.split('-');
+        var returnDate = (newDate[1]) + '-' + newDate[2] + '-' + newDate[0];
+        return returnDate;
+    }
+
+
+
+
     render() {
+        
+        const concert = this.props.concert;
+
+        if (concert._embedded === undefined) {
+            return <h1>Loading...</h1>;
+        }
+        const cityState = concert._embedded.venues[0].city.name + ', ' + concert._embedded.venues[0].state.stateCode;
+        let eventTime = this.convertTime(concert.dates.start.localTime);
+        let convertedDate = this.convertDateFormat(concert.dates.start.localDate);
+        // artist: concertData.name,
+        //     date : concertData.dates.start.localDate,
+        //     time : concertData.dates.start.localTime,
+        //     venue : concertData._embedded.venues[0].name,
+        //     address: concertData._embedded.venues[0].address.line1 + ' ' + concertData._embedded.venues[0].city.name + '' 
+        //     + concertData._embedded.venues[0].state.stateCode + ', ' + concertData._embedded.venues[0].postalCode,
+        //     latitude : concertData._embedded.venues[0].location.latitude,
+        //     longitude : concertData._embedded.venues[0].location.longitude,
+        //     image: concertData.images[0].url,
+
+
+
+
         return (
             <div className="bottom-content">
                 <div className="title">
@@ -11,13 +66,13 @@ class Planner extends Component {
                 </div>
                 <div className="concert-overview">
                     <div>
-                        <h2>Concert: Taylor Swift</h2>
+                        <h2>Concert: {concert.name}</h2>
                     </div>
                     <div>
-                        <h2>Date: May, 24, 2019</h2>
+                        <h2>Date: {convertedDate} @ {eventTime}</h2>
                     </div>
                     <div>
-                        <h2>Location: Anaheim, CA</h2>
+                        <h2>Location: {concert._embedded.venues[0].address.line1 + ', ' + concert._embedded.venues[0].city.name + ''}</h2>
                     </div>
                 </div>
                 <div className="title">WHO'S GOING?</div>
@@ -31,8 +86,6 @@ class Planner extends Component {
                         <h2>HOWARD</h2>
                     </div>
                 </div>
-
-
                 <div className="buttons">
                     <button className="pink-btn">******</button>
                     <button className="white-btn">******</button>
@@ -43,4 +96,10 @@ class Planner extends Component {
     }
 }
 
-export default Planner;
+function mapStateToProps(state) {
+    return {
+        concert: state.concertDetails.concert
+    }
+}
+
+export default connect(mapStateToProps, { get_concert_details: get_concert_details })(Planner);
