0e98bab4f2d62adc3b261c254867a8f89eb69e55
Han-Mi Kim <tmpham1@uci.edu>
2018-08-28 20:33:58 -0700

    test

--- public/api/emailInviteFriends.php
+++ public/api/emailInviteFriends.php
@@ -1,8 +1,28 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 require_once('email_config.php');
-require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+// require_once('mysqlconnect.php');
+require '../../PHPMailer/src/PHPMailer.php';
+require '../../PHPMailer/src/SMTP.php';
+require '../../PHPMailer/src/Exception.php';
+use PHPMailer\PHPMailer\PHPMailer;
+$name = "Howard";
+$query = "www.concertbuddy.app/invited?=";
+// print_r($_SERVER); Ask Scoot and collete to set up information
+function token() {
+ $output = '';
+ $alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+ $alphanumLength = strlen($alphanum);
+ for ($i = 0; $i < $alphanumLength; $i++) {
+    $randomNum = rand(0,$alphanumLength);
+$output .= $alphanum["$randomNum"];
+ }
+ 
+ return $output;
 
+}
 
+$token = token();
 
 $mail = new PHPMailer;          
 $mail->SMTPDebug = 3;                                 // Enable verbose debug output. Change to 0 to disable debugging output.
@@ -21,24 +41,25 @@ $options = array(
     )
 );
 $mail->smtpConnect($options);
-$mail->From = 'concertbuddy@gmail.com';     // sender's email address (shows in "From" field)
+$mail->From = 'concertbuddy.mailserver@gmail.com';     // sender's email address (shows in "From" field)
 $mail->FromName = "Concert Buddy";         // sender's name (shows in "From" field)
-foreach ($_POST['email'] as $value) {
-    $mail->addAddress("$value", "$name");
-} 
+// foreach ($_POST['email'] as $value) {
+//     return $mail->addAddress("$value", "$name");
+// } 
+$mail->addAddress("tmpham1@uci.edu","Tien");
 $mail->addReplyTo('example@gmail.com');    // Add a reply-to address
                                           // Add attachments
                                          // Optional name
 $mail->isHTML(true);                    // Set email format to HTML
 
 $mail->Subject = 'You have been invited to Concert Buddy!';
-$mail->Body    =  "Hello <br>  
-You have been invited to ______'s trip. This will hold all the information in the trip below. 
-Click the link provided below to sign up and join the trip. Welcome to concert buddy!<br>".$POST['query']."<br>"; 
+$mail->Body    = "Hello <br>  
+You have been invited to $name\'s trip. This will hold all the information in the trip below. 
+Click the link provided below to sign up and join the trip. Welcome to concert buddy!<br>".$query.$token."<br>"; 
 
 $mail->AltBody = "Hello <br>  
-You have been invited to ______'s trip. This will hold all the information in the trip below. 
-Click the link provided below to sign up and join the trip. Welcome to concert buddy!<br>".$POST['query']."<br>"; 
+You have been invited to $name\'s trip. This will hold all the information in the trip below. 
+Click the link provided below to sign up and join the trip. Welcome to concert buddy!<br>".$query.$token."<br>"; 
 
 if(!$mail->send()) {
     echo 'Message could not be sent.';
