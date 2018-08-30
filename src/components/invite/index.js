d7be455b85b02912a527404b0d62e1ebbef75da0
Howard Kim <howardkim820@gmail.com>
2018-08-29 21:39:56 -0700

    completed dynamic input rendering

--- src/components/invite/index.js
+++ src/components/invite/index.js
@@ -2,45 +2,66 @@ import React, { Component } from 'react';
 import './invite.css';
 import { reduxForm, Field, FieldArray } from 'redux-form';
 import Input from './input';
-
+import {connect} from 'react-redux';
+import {send_email_invites} from '../../actions'
 
 class InviteFriends extends Component {
     renderEmails(props){
+        //  console.log(props);
         const{fields} = props;
-        console.log(fields);
         const emails = fields.map((name, index) => {
+            if(index < 10){
             return(
-                <Field name={name} label={`Email ${index + 1}`} component={Input} />
+                <Field key ={name} name={name} label={`Email ${index + 1}`} component={Input} />
             )
+        } else {
+            return;
+        }
+            
         })
 
         return (
             <div>
                 {emails}
-                <div type="button" className="add-email" title= "Add Recipient" onClick={()=>{fields.push()}}>
-                    <button>Add</button>
+                <div className="invite-emails" title="Add Recipient" onClick={()=>{fields.push()}}>
+                    <button type="button" className="pink-btn">ADD MORE</button>
                 </div>
             </div>
         )
     }
+    inviteFriends(values){
+        console.log(values.emails);
+        const array = values.emails;
+        for(var i = 0; i < array.length; i++){
+            array[i] = "";
+        }
+        // this.props.send_email_invites(values.emails);
+    }
     submitForm(values){
-        console.log(values)    
+        // console.log(this.props);
+        
     }
     render(){
         const{handleSubmit, reset} = this.props;
         return (
-            <form onSubmit={handleSubmit(this.submitForm)}>
-                <h2>INVITE FRIENDS</h2>
-                <FieldArray name="emails" component = {this.renderEmails} />
-            </form>
+            <div>
+                <form onSubmit={handleSubmit(this.inviteFriends.bind(this))}>
+                    <h2 className="invite-friends">INVITE FRIENDS</h2>
+                    <FieldArray name="emails" component = {this.renderEmails} />
+                    <div style={{textAlign: 'center'}}>
+                        <button className="white-btn">SEND INVITE</button>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
 
-export default reduxForm({
+InviteFriends = reduxForm({
     form: 'invite-friends',
     initialValues: {
         emails: ['']
     }
 
 })(InviteFriends);
+export default connect(null, {send_email_invites})(InviteFriends)
--- src/components/invite/input.js
+++ src/components/invite/input.js
@@ -4,8 +4,7 @@ export default (props) => {
     const {label, input,  meta: {touched, error}} = props;
     return (
         <div>
-            <label>{label}</label>
-            <input className="standard-input" {...input} type="text" />
+            <input placeholder="Enter email address" autoComplete="off" className="standard-input" {...input} type="text" />
         </div>
     )
 }
