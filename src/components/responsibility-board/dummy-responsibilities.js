8a1e5ada5effffcee2c01055fc56597a3da87173
Rebecca Martin <rebecca-martin@live.com>
2018-08-30 14:17:21 -0700

    added email template and made adj to resp board

---
+++ src/components/responsibility-board/dummy-responsibilities.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        title: 'Book Hotel Room',
+        person: 'Howard',
+        details: 'Book five rooms at Hyatt in downtown June 10-15',
+        completed: false
+    },
+
+    {
+        title: 'Bring snacks',
+        person: 'Rebecca',
+        details: 'Bring chips, cookies and candy',
+        completed: true
+    }
+
+];
