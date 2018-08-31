a6132db8fe67c74f0c3753dba03c79ece12ef8a3
Howard Kim <howardkim820@gmail.com>
2018-08-30 19:42:41 -0700

    basic modal component added

--- src/components/invite/index.js
+++ src/components/invite/index.js
@@ -4,9 +4,23 @@ import { reduxForm, Field, FieldArray } from 'redux-form';
 import Input from './input';
 import {connect} from 'react-redux';
 import {send_email_invites} from '../../actions'
-
+import Modal from '../confirmation-modal';
 
 class InviteFriends extends Component {
+    
+    state = {
+        show: false
+    }
+    showModal = () => {
+        this.setState({
+            show: true
+        })
+    }
+    hideModal = () => {
+        this.setState({
+            show: false
+        })
+    }
     renderEmails(props){
         const{fields} = props;
         const emails = fields.map((name, index) => {
@@ -28,6 +42,7 @@ class InviteFriends extends Component {
         )
     }
     inviteFriends(values){
+        this.showModal();
         const array = values.emails; 
         for(var i = 0; i < array.length; i++){
             if(array[i] === undefined){
@@ -35,11 +50,15 @@ class InviteFriends extends Component {
                 console.log(array);
             } 
         }
-
         this.props.send_email_invites(array);
     }
     render(){
         const{handleSubmit, reset} = this.props;
+        const pStyle = {
+            color: 'dodgerblue',
+            fontSize: '32px',
+            textAlign: 'center'
+        }
 
         return (
             <div>
@@ -50,6 +69,9 @@ class InviteFriends extends Component {
                         <button className="white-btn">SEND INVITE</button>
                     </div>
                 </form>
+                <Modal show={this.state.show} handleClose={this.hideModal}>
+                    <p style={pStyle}>Invitations Sent!</p>
+                </Modal>
             </div>
         )
     }
--- src/components/invite/input.js
+++ src/components/invite/input.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default (props) => {
     const {label, input,  meta: {touched, error}} = props; 
     return (
-        <div className="invite-emails">
+        <div>
             <input className="invite-emails" placeholder="Enter email address" autoComplete="off" className="standard-input" {...input} type="text" />
             <p>{touched && error}</p>
         </div>
