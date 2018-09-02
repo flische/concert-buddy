ec8fe23626374f3c225cfb2f57a1a1ce788664ba
Han-Mi Kim <tmpham1@uci.edu>
2018-09-01 19:53:37 -0700

    created endpoint to populate edit fields

---
+++ public/api/get_edit_fields.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+session_start();
+$tripID  = $_POST['trip_id']; //put trip ID youd like to check here , will make dynamic later 
+$editID = $_POST['edit_id'];
+$output = [
+    'success'=> false,
+];
+require_once('mysqlconnect.php');
+$output = [
+    'success'=>false,
+];
+$query = "SELECT * FROM `responsib`
+ WHERE  `trip_id` = '$tripID' AND `ID` = 'edit_id";
+
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+       $output['success'] = true;
+       $row  = mysqli_fetch_assoc($result);
+       $output['data'][] = $row;
+}
+else {
+    $error = mysqli_error($conn);
+    $output['error'] = "Database Error! + $error";
+}
+ $output = json_encode($output);
+ print($output);
+
+?> 
