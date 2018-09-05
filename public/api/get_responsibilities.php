0b34a300240e20db698694dd722728a38503cf5b
Han-Mi Kim <tmpham1@uci.edu>
2018-09-05 11:15:13 -0700

    fixed uncompleted toggle and sorted resp

--- public/api/get_responsibilities.php
+++ public/api/get_responsibilities.php
@@ -10,7 +10,7 @@ $output = [
     'success'=>false,
 ];
 $query = "SELECT * FROM `responsib`
- WHERE  `trip_id` = '$tripID' ";
+ WHERE  `trip_id` = '$tripID' ORDER BY `ID` DESC, `completed` ASC";
 
 $result = mysqli_query($conn, $query);
 
