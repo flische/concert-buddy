adf6a6661bead2af264396fee339d40c92e9e5d0
Howard Kim <howardkim820@gmail.com>
2018-09-05 18:28:16 -0700

    able to get data from token

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const store = createStore(rootReducer, {}, applyMiddleware(think, reduxPromise))
 ReactDOM.render(
     <Provider store={store}>
         <Router>
-            <App />
+            <AcceptancePage />
         </Router>
     </Provider>,
     document.getElementById('root')
