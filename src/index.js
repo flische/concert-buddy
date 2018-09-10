000251b2b9260fb0119032f020c00ba9385bf24c
flische <flische@gmail.com>
2018-09-10 12:23:43 -0700

    added persistent login functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import axios from 'axios';
 import rootReducer from './reducers/index';
 import reduxPromise from 'redux-promise';
 import think from './middleware/think';
@@ -14,7 +15,17 @@ import App from './components/app';
 
 const store = createStore(rootReducer, {}, applyMiddleware(think, reduxPromise));
 
-// Persistent login 
+// ***Persistent login*** // 
+//wraps the entire application as soon as store is created the first time (right above) and checks if user is logged in
+const response =  axios.post('api/checkUserLoggedIn.php').then((response) => { // checks if user is logged in..then...
+    console.log('response', response);
+
+    if (response.data.success) {  // <--- .then( is used to check if user is in fact logged in...
+    // In order to log in, we need to send an action to the reducer! What’s the method that sends actions out? dispatch() !
+        store.dispatch({ type: types.SIGN_IN} );  // if user IS logged in, store.dispatch() -> the SIGN_IN action! 
+    } 
+});
+
 
 ReactDOM.render(
     <Provider store={store}>
