b93d5e89987a40823a7eea97b5b5e796fecf57cf
Howard Kim <howardkim820@gmail.com>
2018-09-13 15:29:03 -0700

    media query for loader done

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -11,4 +11,16 @@ const Loader = () => {
     )
 }
 
+// const Loader = () => {
+//     return (
+//         <div className="spinner">
+//             <div className="rect1"></div>
+//             <div className="rect2"></div>
+//             <div className="rect3"></div>
+//             <div className="rect4"></div>
+//             <div className="rect5"></div>
+//         </div>
+//     )
+// }
+
 export default Loader;
