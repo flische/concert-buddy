941a7cc6238101a387d489f6534a9fe1fc6e183e
Rebecca Martin <rebecca-martin@live.com>
2018-10-15 12:35:15 -0700

    adjusted css for sign in component

--- src/components/sign-up/index.js
+++ src/components/sign-up/index.js
@@ -9,7 +9,7 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        
+
         this.state = {
             form: {
                 email: '',
@@ -33,19 +33,19 @@ class SignUp extends Component {
             show: false
         })
     }
-    
+
     handleChange(event) {
         const { name, value } = event.target;
         const { form } = this.state;
-        const { password, passwordCheck } = this.state.form; 
+        const { password, passwordCheck } = this.state.form;
         this.setState({
-            form: {...form, [name]: value}
+            form: { ...form, [name]: value }
         });
     }
-  
+
     async handleFormSubmit(event) {
         event.preventDefault();
-        const {email, name, password, passwordCheck} = this.state.form;
+        const { email, name, password, passwordCheck } = this.state.form;
         if (password !== passwordCheck) {
             this.setState({
                 form: {
@@ -56,7 +56,7 @@ class SignUp extends Component {
             const dataToSend = {
                 email: email,
                 name: name,
-                password: password 
+                password: password
             };
             const params = formatPostData(dataToSend);
 
@@ -64,33 +64,33 @@ class SignUp extends Component {
                 if (resp.data.success) {
                     this.showModal();
                 };
-             });
+            });
         }
     }
 
     render() {
-        const {email, name, password, passwordCheck, matching} = this.state.form;
+        const { email, name, password, passwordCheck, matching } = this.state.form;
         const correctStyle = {
-            "border" : "2px green solid"
+            "border": "2px green solid"
         };
         const incorrectStyle = {
-            "border" : "2px red solid"
+            "border": "2px red solid"
         };
 
         return (
             <div>
                 <div className="title">SIGN UP FOR <br />CONCERT BUDDY</div>
                 <div className="signup">
-                    <form id="signupForm" onSubmit={(event) => { this.handleFormSubmit(event) } }>
+                    <form id="signupForm" onSubmit={(event) => { this.handleFormSubmit(event) }}>
                         <div className="signup-inputs">
-                            <input type="text" className="standard-input" placeholder="Enter Email Address" name="email" value={email} onChange={this.handleChange}/>
-                            <input type="text" className="standard-input" placeholder="Enter Your Name"  name="name" value={name} onChange={this.handleChange}/>
+                            <input type="text" className="signup-input" placeholder="Enter Email Address" name="email" value={email} onChange={this.handleChange} />
+                            <input type="text" className="signup-input" placeholder="Enter Your Name" name="name" value={name} onChange={this.handleChange} />
                         </div>
                         <div className="signup-inputs">
-                            <input type="text" style={this.state.form.matching ? correctStyle : incorrectStyle} className="standard-input" placeholder="Choose Password" name ="password" type="password" value={password} onChange={this.handleChange} />
-                            <input type="text" style={this.state.form.matching ? correctStyle : incorrectStyle} className="standard-input" placeholder="Confirm Password" name ="passwordCheck" type="password" value={passwordCheck} onChange={this.handleChange}/>
+                            <input type="text" style={this.state.form.matching ? correctStyle : incorrectStyle} className="signup-input" placeholder="Choose Password" name="password" type="password" value={password} onChange={this.handleChange} />
+                            <input type="text" style={this.state.form.matching ? correctStyle : incorrectStyle} className="signup-input" placeholder="Confirm Password" name="passwordCheck" type="password" value={passwordCheck} onChange={this.handleChange} />
                         </div>
-                            <p className={this.state.form.matching ? 'hidden': 'show'}>  Passwords Are not Matching! </p>
+                        <p className={this.state.form.matching ? 'hidden' : 'show'}>  Passwords Are not Matching! </p>
                         <div className="buttons"><button className="pink-btn" >SIGN UP!</button></div>
                     </form>
                 </div>
