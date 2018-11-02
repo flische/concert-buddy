aa91f1f9cc1a0b90d5b25c6df393bef8b2b125c5
Rebecca Martin <rebecca-martin@live.com>
2018-11-02 15:48:37 -0700

    up button on concert results now only appears after user scrolls a certain amount

---
+++ src/components/up/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './up.css';
+
+export default () => {
+
+    return (
+
+        <a href="#top" className="up">
+            <div className="up-btn"><p>UP</p></div>
+        </a>
+    )
+
+
+}
