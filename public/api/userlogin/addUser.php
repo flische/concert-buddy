32929025ebcc9755cd3fc28c9db86f0f7aecb257
Han-Mi Kim <tmpham1@uci.edu>
2018-12-03 22:53:53 -0800

    changed password requirement and changed password showing on login

--- public/api/userlogin/addUser.php
+++ public/api/userlogin/addUser.php
@@ -1,10 +1,17 @@
 <?php 
 $email = $_POST['email'];
+$passwordLength = strlen($_POST['password']);
 $password = sha1($_POST['password']);
 $name = $_POST['name'];
 $output = [
     'success' => false,
 ];
+if ($passwordLength < 8) {
+    $output['error'] = "Password is too short! Please enter at least 8 characters";
+    $output = json_encode($output);
+    print($output);
+    die();
+}
 $emailQuery = "SELECT * FROM `user` WHERE `email` = '$email'";
 $result = mysqli_query($conn, $emailQuery);
  if(mysqli_num_rows($result)) {
